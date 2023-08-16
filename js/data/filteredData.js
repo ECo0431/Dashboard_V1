15da1a970350d7417e66e0cf0b984b5a9516c635
ECo0431 <eliecoffec@gmail.com>
2023-08-16 12:27:34 +0200

    ajout armmWXDAI

--- js/data/filteredData.js
+++ js/data/filteredData.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", function () {
       const csvLines = csvData.split("\n");
       const filteredTableData = [];
 
+      let found = false;
+
       for (const line of csvLines) {
         const row = line.split(",");
         if (
@@ -13,6 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
           row[4] === `"0xd152f549545093347a162dce210e7293f1452150"`
         ) {
           filteredTableData.push(row);
+          found = true;
+        }
+      }
+
+      if (!found) {
+        for (const line of csvLines) {
+          const row = line.split(",");
+          if (row[4] === `"0xdd1d5ea167082a8ec02042991886781acbca35f7"`) {
+            row[7] = row[6]; // Place la valeur de l'index 6 dans l'index 7
+            row[6] = ""; // Réinitialise la valeur de l'index 6
+            filteredTableData.push(row);
+          }
         }
       }
 
@@ -27,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Données filtrées ajoutées au localStorage.");
       } else {
         console.log(
-          'Aucune donnée trouvée avec l\'index 4 "0xf215af7efd2d90f7492a13c3147defd7f1b41a8e" ou "0xd152f549545093347a162dce210e7293f1452150".'
+          'Aucune donnée trouvée avec l\'index 4 "0xf215af7efd2d90f7492a13c3147defd7f1b41a8e", "0xd152f549545093347a162dce210e7293f1452150" ou "0xdd1d5ea167082a8ec02042991886781acbca35f7".'
         );
       }
     } else {
